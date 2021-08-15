4399cb94d2f382b538d26ac13799684d42c21808
Nancy Solanki <56017974+nancysolanki@users.noreply.github.com>
2021-08-15 16:49:06 +0530

    Update Invisible Cloak.py

--- Invisible Cloak.py
+++ Invisible Cloak.py
@@ -6,7 +6,7 @@ cap=cv2.VideoCapture(0)#Read from the default web cam i.e why 0
 time.sleep(3) #for the system to sleep for 3 second before the webcam starts
 for i in range(30):
     retval,back=cap.read()
-back=np.flip(back,axis=1)
+back=np.flip(back,axis=1) #images captured is mirror image so we need to flip it again to get the original background amd axis=1 means laterally invert image
 cap=cv2.VideoCapture(0)  
 
 
@@ -19,7 +19,7 @@ while (cap.isOpened()):  ##Read every Frame from the webcam, until the camera is
     if ret:
         img=np.flip(img,axis=1)
         
-        ##convert the color space from BGR to HSV
+        ##convert the color space from BGR to HSV because BGR  cant detect colour efficiently
         hsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
         
         ##Generat masks to detect red color
